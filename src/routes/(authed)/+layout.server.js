60c187cf5119866b8b8d252eb875a3d3d7dfa811
hicor <hicor@naver.com>
2024-03-08 23:54:25 +0900

    second commit

--- src/routes/(authed)/+layout.server.js
+++ src/routes/(authed)/+layout.server.js
@@ -13,3 +13,4 @@ export async function load({ cookies, url }) {
 	}
 }
 
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 <!------------------ ⬇⬇⬇ HTML 영역 ⬇⬇⬇ ------------------>
 <div tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-200">
     <div class="collapse-title text-xl font-medium">
-      만나서 반갑다 좆밥들아 여기가 홈이다. 즐겨라
+      나이스 투 밑유 여기가 홈이다 즐겨라
     </div>
     <div class="collapse-content"> 
       <p>결국 눌렀구나</p>
